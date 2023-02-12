b08ed2076afb186d2edc4c42a2b29caf344a5ceb
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-02-12 19:39:13 +0200

    task_3

--- index.js
+++ index.js
@@ -8,21 +8,36 @@
 // const pointer = makePizza;
 
 // task_2
-function deliverPizza(pizzaName) {
-  return `Delivering ${pizzaName} pizza.`;
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// // Chande code below this line
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// };
+// console.log(makeMessage("Royal Grand", makePizza));
+
+// task_3
+function makePizza(pizzaName, callback) {
+  console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+  callback(pizzaName);
 }
 
-function makePizza(pizzaName) {
-  return `Pizza ${pizzaName} is being prepared, please wait...`;
-}
+makePizza("Royal Grand", function deliverPizza(pizzaName) {
+  console.log(`Delivering pizza ${pizzaName}.`);
+});
+// Change code below this line
 
-// Chande code below this line
-function makeMessage(pizzaName, callback) {
-  return callback(pizzaName);
-};
+makePizza("Ultracheese", function eatPizza(pizzaName) {
+  
+ console.log("Eating pizza Ultracheese")
+});
 
-
-console.log(makeMessage("Royal Grand", makePizza));
 
 
 
