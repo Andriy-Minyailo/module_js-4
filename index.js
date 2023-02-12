31b0debf2117dc5f1676931d05a67e42680e97ff
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-02-12 19:14:46 +0200

    add task_2

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 // task_1
-function makePizza() {
-  return "Your pizza is being prepared, please wait.";
-}
-// Change code below this line
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+// // Change code below this line
 
-const result = makePizza();
-const pointer = makePizza;
+// const result = makePizza();
+// const pointer = makePizza;
+
+// task_2
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+// Chande code below this line
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+};
+
+
+console.log(makeMessage("Royal Grand", makePizza));
+
+
+
+
+
+
+
