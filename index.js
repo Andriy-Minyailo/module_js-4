7eea989513e6b8e788e43b0b4e6d4c18a86fa71d
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-02-13 20:04:20 +0200

    add task_5

--- index.js
+++ index.js
@@ -38,36 +38,46 @@
 //  console.log("Eating pizza Ultracheese")
 // });
 
-// task_4
-const pizzaPalace = {
-  pizzas: ["Ultracheese", "Smoked", "Four meats"],
-  order(pizzaName,onSuccess, onError) {
-    if (this.pizzas.includes(pizzaName)) {
-      console.log(onSuccess(pizzaName));
-      return onSuccess(pizzaName);
-    } else {
-      console.log(onError(pizzaName));
-      return onError(pizzaName);
-    }
-  },
-};
-// Change code above this line
+// // task_4
+// const pizzaPalace = {
+//   pizzas: ["Ultracheese", "Smoked", "Four meats"],
+//   order(pizzaName,onSuccess, onError) {
+//     if (this.pizzas.includes(pizzaName)) {
+//       console.log(onSuccess(pizzaName));
+//       return onSuccess(pizzaName);
+//     } else {
+//       console.log(onError(pizzaName));
+//       return onError(pizzaName);
+//     }
+//   },
+// };
+// // Change code above this line
 
-// Callback for onSuccess
-function makePizza(pizzaName) {
-  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! There is no pizza with a name ${error} in the assortment.`;
+// }
+
+// // Method calls with callbacks
+// pizzaPalace.order("Smoked", makePizza, onOrderError);
+// pizzaPalace.order("Four meats", makePizza, onOrderError);
+// pizzaPalace.order("Big Mike", makePizza, onOrderError);
+// pizzaPalace.order("Vienna", makePizza, onOrderError);
+
+// task_5
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  // Change code below this line
+orderedItems.forEach(element => totalPrice += element)
+  
+  // Change code above this line
+  return totalPrice;
 }
-
-// Callback for onError
-function onOrderError(error) {
-  return `Error! There is no pizza with a name ${error} in the assortment.`;
-}
-
-// Method calls with callbacks
-pizzaPalace.order("Smoked", makePizza, onOrderError);
-pizzaPalace.order("Four meats", makePizza, onOrderError);
-pizzaPalace.order("Big Mike", makePizza, onOrderError);
-pizzaPalace.order("Vienna", makePizza, onOrderError);
 
 
 
