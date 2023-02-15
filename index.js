5f92ee27eac07e2216e682d4021b4f745229252b
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-02-15 20:18:45 +0200

    add task_16

--- index.js
+++ index.js
@@ -156,28 +156,51 @@
 // const planetsLengths = planets.map(planet => planet.length);
 
 // task_15
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+// // Change code below this line
+
+// const titles = books.map(book => book.title);
+
+// task_16
 const books = [
   {
     title: "The Last Kingdom",
     author: "Bernard Cornwell",
-    rating: 8.38,
+    genres: ["adventure", "history"],
   },
   {
     title: "Beside Still Waters",
     author: "Robert Sheckley",
-    rating: 8.51,
+    genres: ["fiction"],
   },
   {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
+    title: "Redder Than Blood",
+    author: "Tanith Lee",
+    genres: ["horror", "mysticism"],
   },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
 ];
 // Change code below this line
 
-const titles = books.map(book => book.title);
+const genres = books.flatMap(book => book.genres);
+
 
 
 
