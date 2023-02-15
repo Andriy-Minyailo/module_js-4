ac9f0d071eebf638780b90a67da60cc73bee8a65
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-02-15 20:23:46 +0200

    add task_20

--- index.js
+++ index.js
@@ -211,11 +211,35 @@
 // const getUserEmails = users => users.map(user => user.email);
 
 // task_19
-const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
-// Change code below this line
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
 
-const evenNumbers = numbers.filter(number => number % 2 === 0);
-const oddNumbers = numbers.filter(number => number % 2 !== 0);
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 !== 0);
+
+// task_20
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    genres: ["adventure", "history"],
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    genres: ["fiction", "mysticism"],
+  },
+  {
+    title: "Redder Than Blood",
+    author: "Tanith Lee",
+    genres: ["horror", "mysticism", "adventure"],
+  },
+];
+// Change code below this line
+const allGenres = books.flatMap(book => book.genres);
+const uniqueGenres = allGenres.filter(
+  (genre, index, array) => array.indexOf(genre) === index);
+
 
 
 
