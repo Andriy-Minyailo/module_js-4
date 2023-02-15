ca045805ece2cb98bdd7430085da27adcced5488
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-02-15 20:19:46 +0200

    add task_17

--- index.js
+++ index.js
@@ -180,26 +180,32 @@
 // const titles = books.map(book => book.title);
 
 // task_16
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    genres: ["adventure", "history"],
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    genres: ["fiction"],
-  },
-  {
-    title: "Redder Than Blood",
-    author: "Tanith Lee",
-    genres: ["horror", "mysticism"],
-  },
-];
-// Change code below this line
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism"],
+//   },
+// ];
+// // Change code below this line
 
-const genres = books.flatMap(book => book.genres);
+// const genres = books.flatMap(book => book.genres);
+
+// task_17
+const getUserNames = users => {
+    return users.map(user => user.name);
+
+  };
 
 
 
