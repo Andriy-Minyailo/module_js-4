3338173565e7f13c47644e78e650b4813d6eed42
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-02-18 22:45:45 +0200

    add task_30

--- index.js
+++ index.js
@@ -321,11 +321,24 @@
 // const bookByAuthor = books.find(book => book.author === AUTHOR);
 
 // task_29
+// // Change code below this line
+// const getUserWithEmail = (users, email) => users.find(user => user.email === email);
+// // Change code above this line
+
+// task_30
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
 // Change code below this line
-const getUserWithEmail = (users, email) => users.find(user => user.email === email);
-// Change code above this line
 
+const eachElementInFirstIsEven = firstArray.every(number => number % 2 === 0);
+const eachElementInFirstIsOdd = firstArray.every(number => number % 2 !== 0);
 
+const eachElementInSecondIsEven = secondArray.every(number => number % 2 === 0);
+const eachElementInSecondIsOdd = secondArray.every(number => number % 2 !== 0);
+
+const eachElementInThirdIsEven = thirdArray.every(number => number % 2 === 0);
+const eachElementInThirdIsOdd = thirdArray.every(number => number % 2 !== 0);
 
 
 
