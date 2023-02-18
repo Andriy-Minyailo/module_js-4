1b943b8921bd97ec8713076b36dc9eac6534905f
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-02-18 22:46:50 +0200

    add task_31

--- index.js
+++ index.js
@@ -326,20 +326,24 @@
 // // Change code above this line
 
 // task_30
-const firstArray = [26, 94, 36, 18];
-const secondArray = [17, 61, 23];
-const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change code below this line
+
+// const eachElementInFirstIsEven = firstArray.every(number => number % 2 === 0);
+// const eachElementInFirstIsOdd = firstArray.every(number => number % 2 !== 0);
+
+// const eachElementInSecondIsEven = secondArray.every(number => number % 2 === 0);
+// const eachElementInSecondIsOdd = secondArray.every(number => number % 2 !== 0);
+
+// const eachElementInThirdIsEven = thirdArray.every(number => number % 2 === 0);
+// const eachElementInThirdIsOdd = thirdArray.every(number => number % 2 !== 0);
+
+// task_31
 // Change code below this line
-
-const eachElementInFirstIsEven = firstArray.every(number => number % 2 === 0);
-const eachElementInFirstIsOdd = firstArray.every(number => number % 2 !== 0);
-
-const eachElementInSecondIsEven = secondArray.every(number => number % 2 === 0);
-const eachElementInSecondIsOdd = secondArray.every(number => number % 2 !== 0);
-
-const eachElementInThirdIsEven = thirdArray.every(number => number % 2 === 0);
-const eachElementInThirdIsOdd = thirdArray.every(number => number % 2 !== 0);
-
+const isEveryUserActive = (users) => users.every(user => user.isActive);
+// Change code above this line
 
 
 
