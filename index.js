6cc107a0bc7046900a0c6c913bc8801028375b54
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-02-18 22:49:06 +0200

    add task_33

--- index.js
+++ index.js
@@ -346,20 +346,24 @@
 // Change code above this line
 
 // task_32
-const firstArray = [26, 94, 36, 18];
-const secondArray = [17, 61, 23];
-const thirdArray = [17, 26, 94, 61, 36, 23, 18];
-// Change below this line
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change below this line
 
-const anyElementInFirstIsEven = firstArray.some(number => number % 2 === 0);
-const anyElementInFirstIsOdd = firstArray.some(number => number % 2 !== 0);
+// const anyElementInFirstIsEven = firstArray.some(number => number % 2 === 0);
+// const anyElementInFirstIsOdd = firstArray.some(number => number % 2 !== 0);
 
-const anyElementInSecondIsEven = secondArray.some(number => number % 2 === 0);
-const anyElementInSecondIsOdd = secondArray.some(number => number % 2 !== 0);
+// const anyElementInSecondIsEven = secondArray.some(number => number % 2 === 0);
+// const anyElementInSecondIsOdd = secondArray.some(number => number % 2 !== 0);
 
-const anyElementInThirdIsEven = thirdArray.some(number => number % 2 === 0);
-const anyElementInThirdIsOdd = thirdArray.some(number => number % 2 !== 0);
+// const anyElementInThirdIsEven = thirdArray.some(number => number % 2 === 0);
+// const anyElementInThirdIsOdd = thirdArray.some(number => number % 2 !== 0);
 
+// task_33
+// Change code below this line
+const isAnyUserActive = users => users.some(user => user.isActive);
+// Change code above this line
 
 
 
