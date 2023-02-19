72c9be9dd6196ef38b4d3a16dc5d7c7001f871ee
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-02-19 22:33:10 +0200

    add task_34

--- index.js
+++ index.js
@@ -361,10 +361,25 @@
 // const anyElementInThirdIsOdd = thirdArray.some(number => number % 2 !== 0);
 
 // task_33
-// Change code below this line
-const isAnyUserActive = users => users.some(user => user.isActive);
-// Change code above this line
+// // Change code below this line
+// const isAnyUserActive = users => users.some(user => user.isActive);
+// // Change code above this line
 
+// task_34
+const players = {
+  mango: 1270,
+  poly: 468,
+  ajax: 710,
+  kiwi: 244
+};
+const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// Change code below this line
+
+const totalPlayTime = playtimes.reduce(
+  (total, player) => total += player, 0);
+
+// Change code above this line
+const averagePlayTime = totalPlayTime / playtimes.length;
 
 
 
