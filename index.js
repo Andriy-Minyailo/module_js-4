e5ca27634e9a57f9d5763c6687caf5992659c102
Андрій <andrii.minyailo@gmail.com>
2023-02-21 10:47:25 +0200

    add task_41

--- index.js
+++ index.js
@@ -430,22 +430,61 @@
 // );
 
 // task_40
-const authors = [
-  "Tanith Lee",
-  "Bernard Cornwell",
-  "Robert Sheckley",
-  "Fyodor Dostoevsky",
-  "Howard Lovecraft",
+// const authors = [
+//   "Tanith Lee",
+//   "Bernard Cornwell",
+//   "Robert Sheckley",
+//   "Fyodor Dostoevsky",
+//   "Howard Lovecraft",
+// ];
+// // Change code below this line
+
+// const authorsInAlphabetOrder = [...authors].sort(
+//   (a, b) => a.localeCompare(b)
+// );
+
+// const authorsInReversedOrder = [...authors].sort(
+//   (a, b) => b.localeCompare(a)
+// );
+
+// task_41
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
 ];
 // Change code below this line
 
-const authorsInAlphabetOrder = [...authors].sort(
-  (a, b) => a.localeCompare(b)
+const sortedByAuthorName = [...books].sort(
+  (a, b) => a.author.localeCompare(b.author) 
 );
 
-const authorsInReversedOrder = [...authors].sort(
-  (a, b) => b.localeCompare(a)
+const sortedByReversedAuthorName = [...books].sort(
+  (a, b) => b.author.localeCompare(a.author) 
 );
+
+const sortedByAscendingRating = [...books].sort(
+  (a, b) => a.rating - b.rating 
+);
+
+const sortedByDescentingRating = [...books].sort(
+  (a, b) => b.rating - a.rating 
+);;
+
 
 
 
