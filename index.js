a118fe6b11ef0540f7775668a1ad089cabb789af
Андрій <andrii.minyailo@gmail.com>
2023-02-21 10:45:39 +0200

    add task_39

--- index.js
+++ index.js
@@ -404,18 +404,30 @@
 //   (total, user) => total += user.friends.length, 0);
 
 // task_38
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const authors = [
+//   "Tanith Lee",
+//   "Bernard Cornwell",
+//   "Robert Sheckley",
+//   "Fyodor Dostoevsky",
+// ];
+// // Change code below this line
+
+// const ascendingReleaseDates = [...releaseDates].sort();
+
+// const alphabeticalAuthors = [...authors].sort();
+
+// task_39
 const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
-const authors = [
-  "Tanith Lee",
-  "Bernard Cornwell",
-  "Robert Sheckley",
-  "Fyodor Dostoevsky",
-];
 // Change code below this line
 
-const ascendingReleaseDates = [...releaseDates].sort();
+const ascendingReleaseDates = [...releaseDates].sort(
+  (a, b) => a - b
+  );
 
-const alphabeticalAuthors = [...authors].sort();
+const descendingReleaseDates = [...releaseDates].sort(
+  (a, b) => b - a
+);
 
 
 
